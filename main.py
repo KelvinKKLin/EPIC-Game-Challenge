646ea9f6cb31c345f05c2d85108c8f3cb9e5e5b0
LinKelvin <KelvinKKLin@gmail.com>
2014-11-16 14:23:19 -0500

    Working Menus
    Menus for Lesson and Trivia now work.

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ YELLOW = (255, 251, 0)
 MAIN_MENU = 0
 LESSON = 1
 TRIVIA = 2
+LESSON_LEVEL_SELECTION = 3
+TRIVIA_LEVEL_SELECTION = 4
 
 #User Defined Events
 TIMER = 25
@@ -34,6 +36,7 @@ timer = 0
 
 #Used for start menu
 state = MAIN_MENU
+beltIndex = 1
 
 #Whether or not to end the game
 quit = False
@@ -45,8 +48,8 @@ class StartMenu():
         self.screen = screen
         self.bg_colour = bg_colour
         self.clock = pygame.time.Clock()
-        self.lessonButton = graphics.Button(screen, 560, 240, 967, 390, LESSON)
-        self.quizButton = graphics.Button(screen, 206, 406, 543, 543, TRIVIA)
+        self.lessonButton = graphics.Button(screen, 560, 240, 967, 390, LESSON_LEVEL_SELECTION)
+        self.quizButton = graphics.Button(screen, 206, 406, 543, 543, TRIVIA_LEVEL_SELECTION)
     
     def gameLoop(self):
         done = False
@@ -66,6 +69,7 @@ class StartMenu():
             if event.type == pygame.MOUSEBUTTONDOWN :
                 if self.lessonButton.isPressed():
                     state = self.lessonButton.getState()
+                    print state
                 elif self.quizButton.isPressed():
                     state = self.quizButton.getState()
         return state != MAIN_MENU
@@ -77,13 +81,62 @@ class StartMenu():
         graphics.setBackground(self.screen, "Main_Menu.png")
         pygame.display.flip()
         
+class LevelSelectionMenu():
+    def __init__(self, screen, typeOfMenu, nextState, bg_colour = BLACK):
+        self.screen = screen
+        self.bg_colour = bg_colour
+        self.clock = pygame.time.Clock()
+        self.typeOfMenu = typeOfMenu
+        #Implement using for loop(?)
+        self.level = []
+        self.level.append(graphics.Button(screen, 55, 265, 340, 329, nextState))
+        self.level.append(graphics.Button(screen, 55, 369, 340, 432, nextState))
+        self.level.append(graphics.Button(screen, 55, 472, 340, 534, nextState))
+        self.level.append(graphics.Button(screen, 55, 575, 340, 639, nextState))
+        self.level.append(graphics.Button(screen, 376, 265, 660, 329, nextState))
+        self.level.append(graphics.Button(screen, 376, 369, 660, 432, nextState))
+        self.level.append(graphics.Button(screen, 376, 472, 660, 534, nextState))
+        self.level.append(graphics.Button(screen, 697, 265, 980, 329, nextState))
+        self.level.append(graphics.Button(screen, 697, 369, 980, 432, nextState))
+        
+        
+    
+    def gameLoop(self):
+        done = False
+        clock = pygame.time.Clock()
+        while not done:
+            self.updateScreen()
+            done = self.processEvents()
+            clock.tick(60)
+            
+    #Processes user inputs
+    def processEvents(self):
+        global state, quit, beltIndex
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit = True
+                return True
+            if event.type == pygame.MOUSEBUTTONDOWN :
+                for i in range(9):
+                    if self.level[i].isPressed():
+                        state = self.level[i].getState()
+                        beltIndex = i+1
+        return state != self.typeOfMenu
+    
+    
+    #Updates the screen
+    def updateScreen(self):
+        self.screen.fill(BLACK)
+        graphics.setBackground(self.screen, "Level_Menu.png")
+        pygame.display.flip()
+        
         
         
 class PlayQuiz():
     def __init__(self, screen, bg_colour = BLACK):
         self.screen = screen
         self.bg_colour = bg_colour
-        self.questionList, self.selectionList, self.answerList, self.scoreList, self.timeList = wordProcessing.getTriviaScript("Quiz1.txt")        
+        self.questionList, self.selectionList, self.answerList, self.scoreList, self.timeList = wordProcessing.getTriviaScript("Quiz"+str(beltIndex)+".txt")        
         self.clock = pygame.time.Clock()
         
         
@@ -176,7 +229,7 @@ class PlayStory():
     def __init__(self, screen, bg_colour = BLACK):
         self.screen = screen
         self.bg_colour = bg_colour
-        self.backgroundPictureList, self.foregroundPictureList, self.speakerProfilePictureList, self.speakerList, self.dialogList = wordProcessing.getLessonScript("Lesson1.txt")  
+        self.backgroundPictureList, self.foregroundPictureList, self.speakerProfilePictureList, self.speakerList, self.dialogList = wordProcessing.getLessonScript("Lesson"+str(beltIndex)+".txt")  
         self.clock = pygame.time.Clock()
         
     #The game loop runs while the game is not over
@@ -248,6 +301,12 @@ def main():
         if state == MAIN_MENU:
             menu = StartMenu(screen)
             menu.gameLoop()
+        elif state == LESSON_LEVEL_SELECTION:
+            selection = LevelSelectionMenu(screen, LESSON_LEVEL_SELECTION, LESSON)
+            selection.gameLoop()
+        elif state == TRIVIA_LEVEL_SELECTION:
+            selection = LevelSelectionMenu(screen, TRIVIA_LEVEL_SELECTION, TRIVIA)
+            selection.gameLoop()
         elif state == TRIVIA:
             triviaGame = PlayQuiz(screen)
             triviaGame.gameLoop()
@@ -262,7 +321,7 @@ def initializeGame():
     pygame.init()
     size = (1050, 750)
     screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("For I in Python")
+    pygame.display.set_caption("For i in Python")
     return screen
 
 main() #Runs the game
